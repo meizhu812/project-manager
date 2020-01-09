800e3a3d32fddc9f14074ec771c69c4ab828632b
Zhu Mei <meizhu812@outlook.com>
2020-01-09 15:10:41 +0800

    [mz]-feat-#3-AC3 AC4 completed

--- public/index.js
+++ public/index.js
@@ -1,17 +1,19 @@
 import ajax from "../src/ajax.js"
 
 const ROOT_URL = "http://localhost:3000/projects";
-
 const projectCounts = {
   pending: 0,
   active: 0,
   closed: 0,
   all: 0
 };
+const idRowMap = new Map();
 
-function ajaxFailed(text) {
-  console.log("AJAX Failed" + text)
-}
+window.onload = () => {
+  getAllProjects();
+  let projectsList = document.getElementById("projects-list");
+  projectsList.addEventListener("click", handleButtonCLick, false);
+};
 
 function getAllProjects() {
   ajax(
@@ -25,23 +27,28 @@ function getAllProjects() {
       fail: ajaxFailed
     }
   );
-
-
 }
 
 function initProjectsList(allData) {
   let projectsList = document.getElementById("projects-list");
   for (let data of allData) {
-    projectsList.innerHTML +=
-      (`<tr data-id = ${data.id}>`
-        + `<td>${data.name}</td>`
-        + `<td>` + `<p>` + data.description + `</p>` + `</td>`
-        + `<td>` + data.endTime + `</td>`
-        + `<td class="status-${data.status.toLowerCase()}">${data.status.toUpperCase()}</td>`
-        + `<td>` + `<button class="del-btn">删除</button>` + `</td>`
-        + `</tr>`);
+    let rowElement = renderRow(data);
+    projectsList.appendChild(rowElement);
+    idRowMap.set(data.id, rowElement);
     updateProjectsCount(data.status.toLowerCase(), 1);
   }
+}
+
+function renderRow(data) {
+  let row = document.createElement("tr");
+  row.setAttribute("data-id", data.id);
+  row.innerHTML
+    = `<td>${data.name}</td>`
+    + `<td><p>${data.description}</p</td>`
+    + `<td>${data.endTime}</td>`
+    + `<td class="status-${data.status.toLowerCase()}">${data.status.toUpperCase()}</td>`
+    + `<td><button class="del-btn">删除</button></td>`;
+  return row
 }
 
 function updateProjectsCount(statusType, offset) {
@@ -49,40 +56,6 @@ function updateProjectsCount(statusType, offset) {
   projectCounts.all += offset;
   renderOverviewCards()
 }
-
-function handleButtonCLick(event) {
-  let bodyElement = document.body;
-  let target = event.target;
-  if (target.tagName === "BUTTON") {
-    let mask = document.createElement("div");
-    mask.setAttribute("id", "mask");
-    mask.innerHTML
-      = '<div class="confirm-box">'
-      + '<span class="iconfont icon-guanbi" id="close-button"></span>'
-      + '<div class="hint-box">'
-      + '<p class="iconfont icon-wenhao hint-icon"></p>'
-      + '<div class="hint-body">'
-      + '<p class="hint-title">提示</p>'
-      + '<p class="hint-text">确认删除该项目吗?</p>'
-      + '</div></div>'
-      + '<div id="buttons">'
-      + '<button id="confirm-button">确认</button>'
-      + '<button id="cancel-button">取消</button>'
-      + '</div></div>';
-    bodyElement.appendChild(mask);
-    mask.children[0].addEventListener('click', (evt => {
-      switch (evt.target.id) {
-        case("close-button"):
-        case ("cancel-button"):
-          mask.parentElement.removeChild(mask);
-          break;
-      }
-    }))
-
-
-  }
-}
-
 
 function renderOverviewCards() {
   let cards = document.getElementsByClassName("overview-card");
@@ -95,8 +68,65 @@ function renderOverviewCards() {
   }
 }
 
-window.onload = () => {
-  getAllProjects();
-  let projectsList = document.getElementById("projects-list");
-  projectsList.addEventListener("click", handleButtonCLick, false);
-};
+function handleButtonCLick(outEvt) {
+  let bodyElement = document.body;
+  let target = outEvt.target;
+  if (target.tagName === "BUTTON") {
+    let boxWithMask = renderConfirmBox();
+    bodyElement.appendChild(boxWithMask);
+    boxWithMask.children[0].addEventListener('click', (inEvt => {
+      switch (inEvt.target.id) {
+        case("close-button"):
+        case ("cancel-button"):
+          boxWithMask.parentElement.removeChild(boxWithMask);
+          break;
+        case ("confirm-button"):
+          deleteProject(Number(outEvt.target.parentElement.parentElement.getAttribute("data-id")));
+          boxWithMask.parentElement.removeChild(boxWithMask);
+          break;
+      }
+    }))
+  }
+}
+
+function renderConfirmBox() {
+  let boxWithMask = document.createElement("div");
+  boxWithMask.setAttribute("id", "mask");
+  boxWithMask.innerHTML
+    = '<div class="confirm-box">'
+    + '<span class="iconfont icon-guanbi" id="close-button"></span>'
+    + '<div class="hint-box">'
+    + '<p class="iconfont icon-wenhao hint-icon"></p>'
+    + '<div class="hint-body">'
+    + '<p class="hint-title">提示</p>'
+    + '<p class="hint-text">确认删除该项目吗?</p>'
+    + '</div></div>'
+    + '<div id="buttons">'
+    + '<button id="confirm-button">确认</button>'
+    + '<button id="cancel-button">取消</button>'
+    + '</div></div>';
+  return boxWithMask
+}
+
+function deleteProject(id) {
+  ajax({
+    url: ROOT_URL + '/' + id.toString(),
+    method: 'delete',
+    headers: {},
+    data: "",
+    success: removeProjectFromList,
+    successParams: [id],
+    fail: ajaxFailed
+  })
+}
+
+function removeProjectFromList(id) {
+  let target = idRowMap.get(id);
+  let status = target.querySelector('td[class^=status]').innerHTML.toLowerCase();
+  target.parentElement.removeChild(target);
+  updateProjectsCount(status, -1);
+}
+
+function ajaxFailed(text) {
+  console.log("AJAX Failed" + text)
+}
