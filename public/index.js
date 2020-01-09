619186249b5aee86162724d36187ea3db33c71aa
Zhu Mei <meizhu812@outlook.com>
2020-01-09 12:13:25 +0800

    [mz]-feat-#1-AC2 overview rendering completed

--- public/index.js
+++ public/index.js
@@ -2,6 +2,13 @@ import ajax from "../src/ajax.js"
 
 const ROOT_URL = "http://localhost:3000/projects";
 
+const projectCounts = {
+  pending: 0,
+  active: 0,
+  closed: 0,
+  all: 0
+};
+
 function ajaxFailed(text) {
   console.log("AJAX Failed" + text)
 }
@@ -14,22 +21,42 @@ function getAllProjects() {
       params: {},
       headers: {},
       data: "",
-      success: initProjects,
+      success: initProjectsList,
       fail: ajaxFailed
     }
   );
 
-  function initProjects(allData) {
-    let projectsList = document.getElementById("projects-list");
-    for (let data of allData) {
-      projectsList.innerHTML +=
-        (`<tr data-id = ${data.id}>`
-          + `<td>${data.name}</td>`
-          + `<td>` + `<p>` + data.description + `</p>` + `</td>`
-          + `<td>` + data.endTime + `</td>`
-          + `<td class="status-${data.status.toLowerCase()}">${data.status.toUpperCase()}</td>`
-          + `<td>` + `<button class="del-btn">删除</button>` + `</td>`
-          + `</tr>`);
+
+}
+
+function initProjectsList(allData) {
+  let projectsList = document.getElementById("projects-list");
+  for (let data of allData) {
+    projectsList.innerHTML +=
+      (`<tr data-id = ${data.id}>`
+        + `<td>${data.name}</td>`
+        + `<td>` + `<p>` + data.description + `</p>` + `</td>`
+        + `<td>` + data.endTime + `</td>`
+        + `<td class="status-${data.status.toLowerCase()}">${data.status.toUpperCase()}</td>`
+        + `<td>` + `<button class="del-btn">删除</button>` + `</td>`
+        + `</tr>`);
+    updateProjectsCount(data.status.toLowerCase(), 1);
+  }
+}
+
+function updateProjectsCount(statusType, offset) {
+  projectCounts[statusType] += offset;
+  projectCounts.all += offset;
+  renderOverviewCards()
+}
+
+function renderOverviewCards() {
+  let cards = document.getElementsByClassName("overview-card");
+  for (let card of cards) {
+    card.getElementsByClassName("projects-count")[0].innerHTML = projectCounts[card.id.replace("overview-", "")];
+    if (card.id !== "overview-all") {
+      card.getElementsByClassName("projects-percentage")[0].innerHTML
+        = Math.round(projectCounts[card.id.replace("overview-", "")] / projectCounts.all * 100).toString() + "%";
     }
   }
 }
